d985c29ed92beb9c658ca078a112f6d726afc167
ancsaul <ancona.saul01@gmail.com>
2025-02-21 11:38:33 -0600

    starting greatview

--- donut_app_2b/lib/tabs/donut_tab.dart
+++ donut_app_2b/lib/tabs/donut_tab.dart
@@ -1,46 +1,35 @@
-// import 'package:flutter/material.dart';
-
-// class DonutTab extends StatelessWidget {
-  //Lista de Donas
-//   final List donutsOnSale = [ 
-//     //[DoonutFlavor, donutPrice, donutColor, imageName]
-//     ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-//     ["Strawberry", Colors.red,  "lib/images/strawberry_donut.png"],
-//     ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-//     ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-//    ];
-
-//   DonutTab({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GridView.builder(
-//       // Elementos de nuestra lista
-//       itemCount: donutsOnSale.length,
-//       padding: const EdgeInsets.all(18),
-//       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//         //Numero de columnas
-//         crossAxisCount: 2),
-//       itemBuilder: (context, index) {
-//         return DonutTile(
-//           donutFlavor: donutsOnSale[index][0],
-//           donutPrice: donutsOnSale[index][1],
-//           donutColor: donutsOnSale[index][2],
-//           imageName: donutsOnSale[index][3],
-//         );
-//       });
-//   }
-// }
-
+import 'package:donut_app_2b/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class DonutTab extends StatelessWidget {
-  const DonutTab({super.key});
+  //Lista de Donas
+  final List donutsOnSale = [ 
+    //[DoonutFlavor, donutPrice, donutColor, imageName]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "36", Colors.red,  "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+   ];
+
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("DonutTab"),
-    );
+    return GridView.builder( //Grid View parametro para acomodar elementos en una cuadricula
+      // Elementos de nuestra lista
+      itemCount: donutsOnSale.length, //Numero de elementos itemCount
+      padding: const EdgeInsets.all(18),
+      //Organiza como distribuir
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( //
+        //Numero de columnas
+        crossAxisCount: 2),
+      itemBuilder: (context, index) {
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+        );
+      });
   }
 }
