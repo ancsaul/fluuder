f61ffe9b811c0baca6529b0715169c66d37bf91e
ancsaul <ancona.saul01@gmail.com>
2025-03-19 11:39:01 -0600

    adding new products

--- donut_app_2b/lib/tabs/smoothie_tab.dart
+++ donut_app_2b/lib/tabs/smoothie_tab.dart
@@ -1,12 +1,42 @@
+import 'package:donut_app_2b/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class SmoothieTab extends StatelessWidget {
-  const SmoothieTab({super.key});
+  //Lista de Donas
+  final List donutsOnSale = [ 
+    //[doonutFlavor, donutStore, donutPrice, donutColor, imageName]
+    ["Ice Cream", "Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "Dunkin Donuts", "54", Colors.red,  "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "Costco", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "Walmart", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    ["Ice Cream", "Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "Dunkin Donuts", "54", Colors.red,  "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "Costco", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "Walmart", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+   ];
+
+  SmoothieTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("SmoothieTab"),
-    );
+    return GridView.builder( //Grid View parametro para acomodar elementos en una cuadricula
+      // Elementos de nuestra lista
+      itemCount: donutsOnSale.length, //Numero de elementos itemCount
+      padding: const EdgeInsets.all(18),
+      //Organiza como distribuir
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( //
+        //Numero de columnas
+        crossAxisCount: 2,
+        //Relacion de aspecto (proporción de aspecto)
+        childAspectRatio: 1/1.5),
+      itemBuilder: (context, index) {
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutStore: donutsOnSale[index][1],
+          donutPrice: donutsOnSale[index][2],
+          donutColor: donutsOnSale[index][3],
+          imageName: donutsOnSale[index][4],
+        );
+      });
   }
 }
